f5826777cfc4cabed02f08f1c3600919b97eeb81
AgustinLeguizamon <38507913+AgustinLeguizamon@users.noreply.github.com>
2018-06-27 20:46:12 -0300

    Actualizado 27/06
    Devuelve el puntero a arribosAlmacen

--- TP3_PowerRangers_Grafo/src/CaminosMinimos.cpp
+++ TP3_PowerRangers_Grafo/src/CaminosMinimos.cpp
@@ -1,69 +1,74 @@
 /*
  * CaminosMinimos.cpp
  *
- *  Created on: Jun 24, 2018
- *      Author: agustin
+ *  Created on: 27 jun. 2018
+ *      Author: purrevil
  */
 
-
 #include "CaminosMinimos.h"
-//#include "HIIIIIP"
+#include "Cola.h"
 
+Lista<Arribo*>* CaminosMinimos::caminosMinimos(Cola<Arribo*>* heap, Lista<Viaje*>* listaDeAdyacencia){
 
-void CaminosMinimos::caminosMinimos(/*ojo que no es una lista, reemplazar por heap*/
-		Lista<Arribo*>* heap, Lista<Viaje*>* listaDeAdyacencia){
-
+	//busca en la lista de adyacencia, la lista de arribos de almacen
+	Lista<Arribo*>* arribosAlmacen = buscarEnListaDeAdyacenciaPorNombre(listaDeAdyacencia, "ALMACEN");
 
 	while (!heap->estaVacia()){
 
-		/****METODO DEL HEAP****////
+		/*Quita la raiz del heap y la guarda en almacenAProvincia*/
 		Arribo* almacenAProvincia = heap->quitarRaiz();
 
-		//busca en la lista de adyacencia, la lista de arribos de almacen
-		Lista<Arribo*>* arribosAlmacen = buscarEnListaDeAdyacenciaPorNombre(listaDeAdyacencia, "ALMACEN"); //el metodo busca la clase que tenga nombre ALMACEN
-
 		//busca en la lista de adyacencia, la lista de arribos de la provincia que fue removida
-		Lista<Arribo*>* arribosProvincia = buscarEnListaDeAdyacencia(listaDeAdyacencia, almacenAProvincia);
+		Lista<Arribo*>* arribosProvincia = buscarEnListaDeAdyacencia(listaDeAdyacencia,
+				almacenAProvincia);
 
-		//recorre la lista de arribos de la provincia removida
-		arribosProvincia->iniciarCursor();
+		if(arribosProvincia != NULL){
 
-		while(arribosProvincia->avanzarCursor()){
+			//recorre la lista de arribos de la provincia removida
+			arribosProvincia->iniciarCursor();
+
+			while(arribosProvincia->avanzarCursor()){
 
 			/*obtiene el arribo actual de la lista de arribos*/
-			Arribo* provinciaAProvincia = arribosProvincia->obtenerCursor();
+				Arribo* provinciaAProvincia = arribosProvincia->obtenerCursor();
 
 			/*busca la distancia desde almacen hasta la provincia actual
 			en la lista de arribos de Almacen*/
-			Arribo* candidatoAlmacenAProvincia = buscarEnListaDeArribos(arribosAlmacen, provinciaAProvincia);
+				Arribo* candidatoAlmacenAProvincia = buscarEnListaDeArribos(arribosAlmacen, provinciaAProvincia);
 
 
-			if (candidatoAlmacenAProvincia != NULL && elCosteDeLaSumaEsMenorAlCosteDirecto(almacenAProvincia,provinciaAProvincia, candidatoAlmacenAProvincia)){
+				if ( elCosteDeLaSumaEsMenorAlCosteDirecto(almacenAProvincia,
+						provinciaAProvincia, candidatoAlmacenAProvincia)){
 
-				int nuevoCosto= almacenAProvincia->obtenerCosto() + provinciaAProvincia->obtenerCosto();
+					unsigned int nuevoCosto= ((*almacenAProvincia->obtenerCosto()) + (*provinciaAProvincia->obtenerCosto()));
 
 				/****METODO DE LA CLASE ARRIBO ******/
-				candidatoAlmacenAProvincia->modificarCosto(nuevoCosto);
+					candidatoAlmacenAProvincia->cambiarCosto(nuevoCosto);
 
 				/****METODO DEL HEAP******/
-				heap->promover(candidatoAlmacenAProvincia);
+					heap->promover(candidatoAlmacenAProvincia);
 				/**puede ser que la provincia ya no este en el heap, en este caso
 				 * no pasa nada
 				 */
+				}
+
 			}
 
 		}
+
 	}
 
+	return arribosAlmacen;
 }
+
 
 
 bool CaminosMinimos::elCosteDeLaSumaEsMenorAlCosteDirecto(Arribo* almacenAProvincia,
 		Arribo*provinciaAProvincia, Arribo* candidatoAlmacenAProvincia){
 
-	int sumaAlmacenAProvinciayProvinciaAProvincia = almacenAProvincia->obtenerCosto() + provinciaAProvincia->obtenerCosto();
+	unsigned int sumaAlmacenAProvinciayProvinciaAProvincia = (*almacenAProvincia->obtenerCosto()) + (*provinciaAProvincia->obtenerCosto());
 
-	int costeCandidatoAlmacenAProvincia = candidatoAlmacenAProvincia->obtenerCosto();
+	unsigned int costeCandidatoAlmacenAProvincia = (*candidatoAlmacenAProvincia->obtenerCosto());
 
 	return (sumaAlmacenAProvinciayProvinciaAProvincia < costeCandidatoAlmacenAProvincia);
 
@@ -154,18 +159,5 @@ Arribo* CaminosMinimos::buscarEnListaDeArribos(Lista<Arribo*>* listaDeArribos,
 }
 
 
-/*NOTA:son tres metodos de busqueda
- * la primera busca una lista de arribos en la lista de adyacencia
- * la segunda busca una lista de arribos en la lista de adyacencia por nombre string
- * la tercera busca un arribo en una lista de arribos*/
 
 
-/*IMPORTANTE: el codigo actual obtiene las provincias de la lista de almacen
- * si el arribo no esta, devuelve NULL por lo cual nunca va a mejorar el coste
- * de aquellos que no aparezcan desde un principio.
- *
- * Ver de buscar el puntero en el heap y agregarlo a la lista de adyacencia,
- * si no s encuentra en el heap es porque ya salio, devuelve NULL
- * y te ahorras el hacer las cuentas
- *
- */
--- TP3_PowerRangers_Grafo/src/CaminosMinimos.h
+++ TP3_PowerRangers_Grafo/src/CaminosMinimos.h
@@ -11,31 +11,56 @@
 
 #include "Viaje.h"
 #include "Lista.h"
+#include "Cola.h"
+
 
 
 
 class CaminosMinimos{
 
+	private:
+
+	Lista<Arribo*>* copiaDeListaDeArribosAlmacen;
+
 	public:
 
 	/*
-	 * pre:
-	 * post: recibe por parametro el heap con los costes de envio, remueve
-	 * la raiz y actualiza la prioridad de heap.
+	 * post: recibe por parametro el heap con los costes de envio, y la lista
+	 * de adyacencia, remueve la raiz del heap y actualiza los costes
+	 * de los arribos y devuelve la lista de arribo a almacen.
 	 */
 
-	void caminosMinimos(Lista<Arribo*>* heap, Lista<Viaje*>* listaDeAdyacencia);
+	Lista<Arribo*>* caminosMinimos(Cola<Arribo*>* heap, Lista<Viaje*>* listaDeAdyacencia);
 
+	/*
+	 * post: devuelve si la suma del coste de los dos primeros parametros es menor
+	 * al coste del tercer parametro
+	 */
 
 	bool elCosteDeLaSumaEsMenorAlCosteDirecto(Arribo* almacenAProvincia,
 		Arribo*provinciaAProvincia, Arribo* candidatoAlmacenAProvincia);
 
+	/*
+	 *post: busca en la lista de adyacencia un puntero a un arribo
+	 * pasado por parametro y lo devuelve.
+	 */
 
-	Lista<Arribo*>* buscarEnListaDeAdyacencia(Lista<Viaje*>* listaDeAdyacencia, Arribo* unArribo);
+	Lista<Arribo*>* buscarEnListaDeAdyacencia(Lista<Viaje*>* listaDeAdyacencia,
+												Arribo* unArribo);
+
+	/*
+	 * post: busca en la lista de adyacencia un puntero a un arribo,
+	 * utilizando el nombre del arribo como criterio de busqueda y lo devuelve
+	 */
 
 	Lista<Arribo*>* buscarEnListaDeAdyacenciaPorNombre(Lista<Viaje*>* listaDeAdyacencia, std::string nombreProvincia);
 
+	/*
+	 * post: busca en la lista de arribos un puntero a un arribo y lo devuelve
+	 */
+
 	Arribo* buscarEnListaDeArribos(Lista<Arribo*>* listaDeArribos, Arribo* unArribo);
+
 
 
 };
